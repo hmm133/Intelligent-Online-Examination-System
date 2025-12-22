6ed18e3a82a812084f4a11b59370c05182b5ab6c
hmm133 <1484586642@qq.com>
2025-12-22 13:53:30 +0800

    chore: 初始化后端项目骨架

---
+++ backend/src/main/java/cn/org/alan/exam/ExamApplication.java
@@ -0,0 +1,6 @@
+﻿@SpringBootApplication
+public class ExamApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(ExamApplication.class, args);
+    }
+}
